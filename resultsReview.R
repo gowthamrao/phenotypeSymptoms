cc103710e79463715677843a6ac141ee1f237a17
Gowtham Rao <rao@ohdsi.org>
2023-06-05 17:02:40 -0400

    Update resultsReview.R

--- resultsReview.R
+++ resultsReview.R
@@ -1,84 +1,18 @@
 filePath <- dirname(rstudioapi::getActiveDocumentContext()$path)
 source(file.path(filePath, "00_common.R"))
-source(file.path(filePath, "01_selected_phenotypes.R"))
-source(file.path(filePath, "02_generate_cohorts.R"))
-# 
-fileNames <- dplyr::tibble(
-  folders = list.files(
-    path = "D:\\studyResults\\symptomStudy\\CovariateCohort",
-    full.names = TRUE,
-    include.dirs = TRUE,
-    pattern = "cdm_optum_extended_dod_v2434",
-    recursive = TRUE
-  )
-) |>
-  dplyr::mutate(
-    primaryCohortId = stringr::str_replace(
-      string = folders,
-      pattern = stringr::fixed("D:\\studyResults\\symptomStudy\\CovariateCohort/"),
-      replacement = ""
-    )
-  ) |>
-  dplyr::mutate(
-    primaryCohortId = stringr::str_replace(
-      string = primaryCohortId
-      ,
-      pattern = stringr::fixed("/cdm_optum_extended_dod_v2434"),
-      replacement = ""
-    )
-  )
+cohortsToStudy <- readRDS(file = file.path(filePath,
+                                           "cohortsToStudy.RDS"))
+cohortDefinitionSet <- readRDS(file = file.path(filePath,
+                                                "cohortDefinitionSet.RDS"))
+databaseIds <- PrivateScripts::getListOfDatabaseIds()
 
 
-i = 1
-analysisRef <- readRDS(file = file.path(fileNames[i,]$folders,
-                                        "analysisRef.RDS"))
-covariateRef <- readRDS(file = file.path(fileNames[i,]$folders,
-                                        "covariateRef.RDS"))
-covariates <- readRDS(file = file.path(fileNames[i,]$folders,
-                                        "covariates.RDS"))
-timeRef <- readRDS(file = file.path(fileNames[i,]$folders,
-                                    "timeRef.RDS"))
+resultsFolder <- "D:\\studyResults\\symptomStudy\\CovariateCohorts\\Consolidated\\byPrimaryCohortId\\26"
 
+characterization <- readFeatureExtractionOutput(inputFolder = resultsFolder)
 
-covariates |>
-  dplyr::inner_join(timeRef) |>
-  dplyr::inner_join(covariateRef) |>
-  dplyr::inner_join(analysisRef) |>
-  dplyr::mutate(covariateCohortCohortId = (covariateId - analysisId) / 1000) |>
-  # dplyr::filter(cohortId == cohortId) |>
-  dplyr::select(
-    cohortId,
-    startDay,
-    endDay,
-    covariateCohortCohortId,
-    conceptId,
-    analysisId,
-    covariateId,
-    cohortId,
-    covariateName,
-    analysisName,
-    domainId,
-    mean,
-    sumValue
-  ) |>
-  dplyr::filter(mean > 0.01) |>
-  dplyr::mutate(mean = round(x = mean, digits = 2)) |>
-  # dplyr::filter(analysisName == 'ConditionEraGroupOverlap') |>
-  dplyr::filter(startDay == 0) |>
-  dplyr::filter(endDay == 0) |>
-  dplyr::collect() |>
-  dplyr::filter(stringr::str_ends(string = as.character(cohortId),
-                                  pattern = "001")) |>
-  dplyr::inner_join(cohortDefinitionSet |>
-                      dplyr::select(cohortId,
-                                    cohortName)) |>
-  dplyr::arrange(dplyr::desc(mean)) |>
-  dplyr::relocate(cohortName) |>
-  dplyr::select(cohortName, 
-                cohortId,
-                covariateCohortCohortId,
-                covariateName,
-                mean,
-                sumValue) |> 
+createFeatureExtractionReport(characterization = characterization) |> 
   View()
+
+
 
